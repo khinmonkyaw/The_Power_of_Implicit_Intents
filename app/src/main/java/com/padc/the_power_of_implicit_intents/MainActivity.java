d558d6cfdca2207cea393ff5f8b22d01a3c83313
Khin Mon Kyaw <khinmonkyaw95@gmail.com>
2019-02-22 20:18:48 +0630

    Initial Commit

---
+++ app/src/main/java/com/padc/the_power_of_implicit_intents/MainActivity.java
@@ -0,0 +1,261 @@
+package com.padc.the_power_of_implicit_intents;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.AlarmClock;
+import android.provider.CalendarContract;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_VIDEO_CAPTURE = 1;
+    private static final int REQUEST_SELECT_CONTACT =2 ;
+    Uri videoUrl;
+    private Button btnTimer, btnCalender, btnVideo, btnPhoneContact, btnWeb;
+    private VideoView vvVideo;
+    private TextView tv_name, tv_no;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        vvVideo =findViewById(R.id.vv_video);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        //Timer
+        btnTimer = findViewById(R.id.btn_timer);
+        btnTimer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Timer", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(AlarmClock.ACTION_SET_TIMER)
+                        .putExtra(AlarmClock.EXTRA_SKIP_UI, false);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+
+
+            }
+        });
+
+        //Calender
+        btnCalender = findViewById(R.id.btn_calender);
+        btnCalender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Calender", Toast.LENGTH_SHORT).show();
+
+                Calendar calendar = Calendar.getInstance();
+
+                String title = "Barcamp";
+                String location = "MICT PARK";
+                Intent intent = new Intent(Intent.ACTION_INSERT)
+                        .setData(CalendarContract.Events.CONTENT_URI)
+                        .putExtra(CalendarContract.Events.TITLE, title)
+                        .putExtra(CalendarContract.Events.EVENT_LOCATION, location)
+                        .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, calendar.getTimeInMillis())
+                        .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, calendar.getTimeInMillis() + 60 * 60 * 1000);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+
+
+        //Capture a video with implicit intent and show it back in App with auto-play (need to use VideoView).
+        btnVideo = findViewById(R.id.btn_video);
+        btnVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "video", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                startActivityForResult(intent,REQUEST_VIDEO_CAPTURE);
+
+            }
+        });
+
+        //Phone Contact
+        btnPhoneContact = findViewById(R.id.btn_phone_contact);
+        tv_name = findViewById(R.id.tv_name);
+        tv_no = findViewById(R.id.tv_phone_number);
+        btnPhoneContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "phone contact", Toast.LENGTH_SHORT).show();
+                selectContact();
+            }
+        });
+
+
+
+
+        btnWeb = findViewById(R.id.btn_web);
+        btnWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWebPage("https://www.aliexpress.com");
+            }
+        });
+
+
+    }
+
+    public void openWebPage(String url) {
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+
+    public void selectContact() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(ContactsContract.Contacts.CONTENT_TYPE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_SELECT_CONTACT);
+        }
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if(requestCode == REQUEST_SELECT_CONTACT)
+        {
+            String phoneNo="",name ="";
+            int index;
+            Uri contactUri = data.getData();
+            Cursor cursorName = getContentResolver().query(contactUri,null,null,null,null);
+           // Cursor cursorPhoneNo = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,null);
+            if(cursorName.moveToFirst())
+            {
+                index = cursorName.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
+                name = cursorName.getString(index);
+
+                String contactId =
+                        cursorName.getString(cursorName.getColumnIndex(ContactsContract.Contacts._ID));
+
+                Cursor phones =  getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + contactId, null, null);
+                while (phones.moveToNext()) {
+                    String number = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    phoneNo = number;
+                    int type = phones.getInt(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.TYPE));
+                    switch (type) {
+                        case ContactsContract.CommonDataKinds.Phone.TYPE_HOME:
+                            // do something with the Home number here...
+                            break;
+                        case ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE:
+                            // do something with the Mobile number here...
+                            Log.d("ContactsH", number);
+                            break;
+                        case ContactsContract.CommonDataKinds.Phone.TYPE_WORK:
+                            // do something with the Work number here...
+                            break;
+                    }
+                }
+                phones.close();
+
+            }
+
+//            if(cursorPhoneNo.moveToFirst())
+//            {
+//                index = cursorPhoneNo.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DATA);
+//                phoneNo = cursorPhoneNo.getString(index);
+//            }
+
+            tv_name.setText(name);
+            tv_no.setText(phoneNo);
+
+
+
+
+
+
+        }
+        //video capture cancel or ok
+        else if (requestCode == REQUEST_VIDEO_CAPTURE) {
+            if (resultCode == RESULT_OK) {
+                Uri contactUri = data.getData();
+                vvVideo.setVideoURI(contactUri);
+                vvVideo.start();
+                Log.d("MainAc",contactUri.toString());
+                Toast.makeText(this, "Video saved to:\n" +
+                        data.getData(), Toast.LENGTH_LONG).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Video recording cancelled.",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Failed to record video",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
